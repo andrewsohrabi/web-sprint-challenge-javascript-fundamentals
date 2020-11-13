2c3d25e427d729b5ce0cc8a4affb18dc8999d097
Andrew Sohrabi <andrewsohrabi@Andrews-MacBook-Pro.local>
2020-11-13 09:12:13 -0800

    task 1, 2 done

--- index.js
+++ index.js
@@ -17,15 +17,20 @@ myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
 
-
+// nestedFunction can access "internal" because of closure, which allows nested functions to access variables defined in their parent scopes
 
 
 
 /* 🚀🚀🚀 Task 2: Counter 🚀🚀🚀 */
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
-function summation(/*Your Code Here*/) {
+function summation(number) {
   /*Your Code Here*/
-
+  let counter = 0; 
+  for (let  i=0; i <= number;i++) {
+      console.log(counter)
+      counter += i;
+    }
+    return counter;
   }
  
 
